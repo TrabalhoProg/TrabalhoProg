75f1c66619aff049c245f8e577577cc9934b4f1d
BRUNO BRAGA BEDULATTO <bruno.bbedulatto@senacsp.edu.br>
2017-08-28 21:58:18 -0300

    Commit Inicial

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Model;
+
+public class GameController : MonoBehaviour {
+
+    GameModel gameModel;
+
+    private void Start()
+    {
+        Inicializar();
+    }
+
+    public void Inicializar()
+    {
+        gameModel = new GameModel();
+    }
+
+}
---
+++ Assets/Scripts/Model/GameModel.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class Tiro
+    {
+        public GameObject tiroObject;
+        public float massa, densidade;
+    }
+
+    public class Player
+    {
+        int _escudo, _maxMovement;
+        Queue<Tiro> _tiros;
+
+        public Player()
+        {
+            _escudo = 3;
+            _maxMovement = 5;
+            _tiros = new Queue<Tiro>();
+        }
+
+        public int Escudo
+        {
+            get
+            {
+                return _escudo;
+            }
+        }
+
+        public int MaxMovement
+        {
+            get
+            {
+                return _maxMovement;
+            }
+        }
+
+        public void RetirarEscudo()
+        {
+            if (_escudo > 0)
+                _escudo--;
+            else
+                Morrer();
+        }
+
+        public void RetirarMaxMovement()
+        {
+            if (MaxMovement > 0)
+            {
+                _maxMovement--;
+            }
+        }
+
+        public void AumentarMaxMovement()
+        {
+            _maxMovement++;
+        }
+
+        public void Atirar(float angulo, float forca, float vento)
+        {
+
+        }
+
+        public void Morrer()
+        {
+
+        }
+    }
+
+    public class GameModel : MonoBehaviour
+    {
+
+        public Player player1, player2;
+
+        public GameModel()
+        {
+            player1 = new Player();
+            player2 = new Player();
+        }
+
+    }
+}
+
